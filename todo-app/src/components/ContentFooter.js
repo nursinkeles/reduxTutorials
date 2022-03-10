a83da453362a7dee4cb8e21c563faaed2c861825
unknown <nur-sin_sena@hotmai.com>
2022-03-10 19:11:01 +0300

    redux store and actions have been added

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,27 +1,55 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { changeActiveFilter, clearCompleted } from "../redux/todos/todosSlice";
 
 function ContentFooter() {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.todos.items);
+  const itemsLeft = items.filter((item) => !item.completed).length;
+  console.log(itemsLeft);
+
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
+
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>2</strong>
+        <strong>{itemsLeft}</strong>
         {"  "}
-        items left
+        item{itemsLeft > 1 && "s"} left
       </span>
 
       <ul className="filters">
         <li>
-          <a className="selected">All</a>
+          <a
+            className={activeFilter === "all" ? "selected" : ""}
+            onClick={() => dispatch(changeActiveFilter("all"))}
+          >
+            All
+          </a>
         </li>
         <li>
-          <a>Active</a>
+          <a
+            className={activeFilter === "active" ? "selected" : ""}
+            onClick={() => dispatch(changeActiveFilter("active"))}
+          >
+            Active
+          </a>
         </li>
         <li>
-          <a>Completed</a>
+          <a
+            className={activeFilter === "completed" ? "selected" : ""}
+            onClick={() => dispatch(changeActiveFilter("completed"))}
+          >
+            Completed
+          </a>
         </li>
       </ul>
 
-      <button className="clear-completed">Clear completed</button>
+      <button
+        className="clear-completed"
+        onClick={() => dispatch(clearCompleted())}
+      >
+        Clear completed
+      </button>
     </footer>
   );
 }
