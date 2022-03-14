0bb03e1c941eaf86d2ba879b28bd85647a394030
unknown <nur-sin_sena@hotmai.com>
2022-03-14 15:56:41 +0300

    selectors

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,9 +1,13 @@
 import { useSelector, useDispatch } from "react-redux";
-import { changeActiveFilter, clearCompleted } from "../redux/todos/todosSlice";
+import {
+  changeActiveFilter,
+  clearCompleted,
+  selectTodos,
+} from "../redux/todos/todosSlice";
 
 function ContentFooter() {
   const dispatch = useDispatch();
-  const items = useSelector((state) => state.todos.items);
+  const items = useSelector(selectTodos);
   const itemsLeft = items.filter((item) => !item.completed).length;
   console.log(itemsLeft);
 
